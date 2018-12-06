6aad34a754865a482531afc3bb14b2b8fdddee3c
rubick123 <396641508@qq.com>
2018-12-06 20:03:58 +0800

    65

--- model/src/components/register-input.vue
+++ model/src/components/register-input.vue
@@ -1,16 +1,13 @@
 <template>
   <el-form :inline="true" :model="formInline" class="demo-form-inline">
-    <el-form-item label="审批人">
-      <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+    <el-form-item label="账户">
+      <el-input v-model="formInline.user" placeholder="账户名"></el-input>
     </el-form-item>
-    <el-form-item label="活动区域">
-      <el-select v-model="formInline.region" placeholder="活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
+    <el-form-item label="密码">
+      <el-input v-model="formInline.user" placeholder="密码"></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="onSubmit">查询</el-button>
+      <el-button type="primary">登陆</el-button>
     </el-form-item>
   </el-form>
 
@@ -29,7 +26,7 @@
       methods: {
         onSubmit() {
           console.log('submit!');
-        }
+        },
       }
     }
 </script>
--- model/src/router/index.js
+++ model/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ManagerInterface from '@/components/manager-interface.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/manager-interface',
+      name:'manager-interface',
+      component:ManagerInterface,
     }
   ]
 })
